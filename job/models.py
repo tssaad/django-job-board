4cc13a4177f7132f4bb8fe2c87ebb88a9b44e5c2
tssaad <87897916+tssaad@users.noreply.github.com>
2021-07-24 16:52:52 +0200

    add job models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+
+JOB_TYPES = (
+    ('Full Time','Full Time'),
+    ('Part Time','Part Time'),
+)
+class Job(models.Model):
+    JOBTitle = models.CharField(max_length=100, verbose_name=_("Title"))
+    # location = 
+    JOBJobtype = models.CharField(max_length=15, choices=JOB_TYPES,verbose_name=_('Job Type'))
+    JOBDescription = models.TextField(max_length=1000, verbose_name=_("Description"))
+    JOBCreated_at = models.DateTimeField(auto_now=True, verbose_name=_("Date Created"))
+    JOBVacancy = models.IntegerField(default=1, verbose_name=_("Vacancy"))
+    JOBSalary = models.IntegerField(default=0, verbose_name=_("Salary"))
+    JOBExperience = models.IntegerField(default=1, verbose_name=_("Experience"))
+
+    class Meta:
+        verbose_name= _("Job")
+        verbose_name_plural = _("Jobs")
+
+    def __str__(self):
+        return self.JOBTitle
